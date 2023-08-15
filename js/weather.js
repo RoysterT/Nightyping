30090b956903d460e6eb09dc95bb5cc5d2b522cc
Royster <github@royster.email>
2023-08-15 10:40:30 +0000

    Site updated: 2023-08-15 10:40:30

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "10",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "12",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "8440bbfca0614a0e94ee62aafa17a26a"
+  }
+}
