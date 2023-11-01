d52f2cab793ccabd9747dfae26d9574d8928fbf6
RoysterT <os@royster.email>
2023-11-01 09:56:53 +0000

    0.4.7_beta (Auto deploy by Github Action)

--- js/kslink.js
+++ js/kslink.js
@@ -1,7 +1,7 @@
 var leonus = {
     linkCom: e => {
         var t = document.querySelector(".el-textarea__inner");
-        "change" == e ? (t.value = "- 原站点地址（必填）：\n- 修改后信息（仅填写修改项即可）\n  - 站点名称：\n  - 站点地址：\n  - 站点描述：", t.setSelectionRange(15, 15)) : "bf" == e ? (t.value = "```yml\n", t.value += "- name: \n  link: \n  avatar: \n  descr: \n  siteshot: ", t.value += "\n```", t.setSelectionRange(15, 15)) : (t.value = "- 站点名称：\n- 站点地址：\n- 头像链接：\n- 站点描述：\n- 站点预览：", t.setSelectionRange(5, 5)), t.focus()
+        "change" == e ? (t.value = "- 原站点地址（必填）：\n- 修改后信息（仅填写修改项即可）\n  - 站点名称：\n  - 站点地址：\n  - 站点描述：\n  - 站点图标：\n  - 站长头像：", t.setSelectionRange(15, 15)) : "bf" == e ? (t.value = "```yml\n", t.value += "- name: \n  link: \n  avatar: \n  descr: \n  siteshot: ", t.value += "\n```", t.setSelectionRange(15, 15)) : (t.value = "- 站点名称：\n- 站点地址：\n- 头像链接：\n- 站点描述：\n- 站点预览：", t.setSelectionRange(5, 5)), t.focus()
     },
     owoBig: () => {
         if (!document.getElementById("post-comment") || document.body.clientWidth < 768) return;
