eccb4498b37fd7e6d2f618f8905b2580886769cd
Royster <os@royster.email>
2024-02-28 21:36:55 +0800

    v0.5.9_beta

--- js/fomal.js
+++ js/fomal.js
@@ -2,7 +2,7 @@
 // 全局变量
 
 // 版本号
-var latestVersion = "0.5.8_beta";
+var latestVersion = "0.5.9_beta";
 
 //----------------------------------------------------------------
 
@@ -5107,126 +5107,7 @@ function clearCache() {
 
 /* 关于我页面js */
 
-function initAboutPage() {
-  const ck = "3FI49W6PtxRahPCr";
-  var laUrl = "https://v6-widget.51.la/v6/" + ck + "/quote.js";
-  fetch(laUrl)
-    .then(res => res.text())
-    .then(data => {
-      let title = ["最近活跃", "今日人数", "今日访问", "昨日人数", "昨日访问", "本月访问", "总访问量"];
-      let num = data.match(/(<\/span><span>).*?(\/span><\/p>)/g);
-      num = num.map(el => {
-        let val = el.replace(/(<\/span><span>)/g, "");
-        let str = val.replace(/(<\/span><\/p>)/g, "");
-        return str;
-      });
-      let statisticEl = document.getElementById("statistic");
-      // 自定义不显示哪个或者显示哪个，如下为不显示 最近活跃访客 和 总访问量
-      let statistic = [];
-      for (let i = 0; i < num.length; i++) {
-        if (!statisticEl) return;
-        if (i == 0) continue;
-        statisticEl.innerHTML +=
-          "<div><span>" + title[i] + "</span><span id=" + title[i] + ">" + num[i] + "</span></div>";
-        queueMicrotask(() => {
-          statistic.push(
-            new CountUp(title[i], 0, num[i], 0, 2, {
-              useEasing: true,
-              useGrouping: true,
-              separator: ",",
-              decimal: ".",
-              prefix: "",
-              suffix: "",
-            })
-          );
-        });
-      }
-      let statisticElement = document.querySelector(".about-statistic.author-content-item");
-      function statisticUP() {
-        if (!statisticElement) return;
-        const callback = (entries, observer) => {
-          entries.forEach(entry => {
-            if (entry.isIntersecting) {
-              for (let i = 0; i < num.length; i++) {
-                if (i == 0) continue;
-                queueMicrotask(() => {
-                  statistic[i - 1].start();
-                });
-              }
-              observer.disconnect(); // 停止观察元素，因为不再需要触发此回调
-            }
-          });
-        };
-        const options = {
-          root: null,
-          rootMargin: "0px",
-          threshold: 0
-        };
-        const observer = new IntersectionObserver(callback, options);
-        observer.observe(statisticElement);
-      }
-      const selfInfoContentYear = new CountUp("selfInfo-content-year", new Date().getFullYear(), 2003, 0, 2, {
-        useEasing: true,
-        useGrouping: false,
-        separator: ",",
-        decimal: ".",
-        prefix: "",
-        suffix: "",
-      });
-      let selfInfoContentYearElement = document.querySelector(".author-content-item.selfInfo.single");
-      function selfInfoContentYearUp() {
-        if (!selfInfoContentYearElement) return;
-
-        const callback = (entries, observer) => {
-          entries.forEach(entry => {
-            if (entry.isIntersecting) {
-              selfInfoContentYear.start();
-              observer.disconnect(); // 停止观察元素，因为不再需要触发此回调
-            }
-          });
-        };
-        const options = {
-          root: null,
-          rootMargin: "0px",
-          threshold: 0
-        };
-        const observer = new IntersectionObserver(callback, options);
-        observer.observe(selfInfoContentYearElement);
-      }
-      selfInfoContentYearUp();
-      statisticUP()
-    });
-  var pursuitInterval = null;
-  pursuitInterval = setInterval(function () {
-    const show = document.querySelector("span[data-show]");
-    const next = show.nextElementSibling || document.querySelector(".first-tips");
-    const up = document.querySelector("span[data-up]");
-    if (up) {
-      up.removeAttribute("data-up");
-    }
-    show.removeAttribute("data-show");
-    show.setAttribute("data-up", "");
-
-    next.setAttribute("data-show", "");
-  }, 2000);
-  document.addEventListener("pjax:send", function () {
-    pursuitInterval && clearInterval(pursuitInterval);
-  });
-  var helloAboutEl = document.querySelector(".hello-about");
-  helloAboutEl.addEventListener("mousemove", evt => {
-    const mouseX = evt.offsetX;
-    const mouseY = evt.offsetY;
-    gsap.set(".cursor", {
-      x: mouseX,
-      y: mouseY,
-    });
-    gsap.to(".shape", {
-      x: mouseX,
-      y: mouseY,
-      stagger: -0.1,
-    });
-  });
-}
+// initAboutPage()函数放置到about页面中了，因为局部加载是异步的，所以需要在页面加载完成后执行
 
 /* 关于我页面js end */
 
